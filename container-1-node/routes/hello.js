6c4bc4a7587bf43359707c40e1b5e541c34900ad
Jeff Hollan <jeffhollan@outlook.com>
2021-11-02 16:23:16 -0700

    adding axios and fixing bicep command

--- container-1-node/routes/hello.js
+++ container-1-node/routes/hello.js
@@ -5,10 +5,17 @@ const dotnetFQDN = process.env.DOTNET_FQDN;
 
 /* GET users listing. */
 router.get('/', async  function(req, res, next) {
-  // Even though we use the FQDN, because both containers are in the 
-  // same environment, traffic will not leave the environment.
-  var data = await axios.get(`http://${dotnetFQDN}`);
-  res.send(`${JSON.stringify(data.data)}`);
+
+  if(dotnetFQDN)  {
+    // Even though we use the FQDN, because both containers are in the 
+    // same environment, traffic will not leave the environment.
+    var data = await axios.get(`http://${dotnetFQDN}`);
+    res.send(`${JSON.stringify(data.data)}`);
+  }
+  else {
+    res.send('No DOTNET_FQDN env variable defined. Be sure to set an env variable for the dotnetApp FQDN')
+  }
+
 });
 
 module.exports = router;
